04db5f382b4d288a38ae103fb2c86f94c175df43
Ruarc Sorensen <ruarcs@gmail.com>
2015-06-04 14:51:41 +0100

    Add setup file and move stuff around.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='iverifier',
+      version='0.1',
+      description='Simple zope.interface verifier',
+      author='Ruarc Sorensen',
+      author_email='ruarcs@gmail.com',
+      url='https://github.com/ruarcs/iverifier',
+      requires=['zope.interface']
+     )
