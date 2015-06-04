affb2747f9ea2a30ede2225adfaf591f7fe9d0ff
Ruarc Sorensen <ruarcs@gmail.com>
2015-06-04 15:02:51 +0100

    Add an explaning comment.

--- demo/connector.py
+++ demo/connector.py
@@ -7,6 +7,11 @@ class Connector(object):
     def __init__(self,x=None):
         self.x = x
 
+    # Yes, this is meant to be here.
+    # With this in place the utility will
+    # flag this method as missing. Try
+    # putting it in and you'll see the
+    # error going away.
 #    def connect( b ):
 #        pass
 
